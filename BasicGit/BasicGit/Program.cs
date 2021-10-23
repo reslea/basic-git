2b96ff7fe1d8a5d74fa2e77fa23515647ba915a5
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-10-23 09:39:24 +0300

    empty console project

---
+++ BasicGit/BasicGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!!!!!!!!!!!!!!!!!");
+        }
+    }
+}
